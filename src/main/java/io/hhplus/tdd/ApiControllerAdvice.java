b6bfae0773d45cf3f5013d25ad94608c22579327
ohhyoungseok <gudtjr1350@gmail.com>
2024-12-18 00:04:07 +0900

    feat : ControllerAdvice에 IllegalArgumentException 핸들러 추가

--- src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
+++ src/main/java/io/hhplus/tdd/ApiControllerAdvice.java
@@ -11,4 +11,11 @@ class ApiControllerAdvice extends ResponseEntityExceptionHandler {
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         return ResponseEntity.status(500).body(new ErrorResponse("500", "에러가 발생했습니다."));
     }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorResponse("400", e.getMessage()));
+    }
 }
